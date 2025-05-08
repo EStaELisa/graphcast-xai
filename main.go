7466520d213d8bfa37b65bb10f659a0c91ef4b45
Esta Kempf <esta.kempf@gmail.com>
2025-05-08 15:12:04 +0200

    Add GraphCast input preparation notebook and update dependencies
    - Created `prepare_graphcast_input.ipynb` for downloading and processing weather data for GraphCast.
    - Added required packages to `requirements/base.txt`, including Jupyter, cdsapi, and GraphCast.
    - Updated `requirements/local.txt` and `requirements/server.txt` to include base dependencies.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	iam "github.com/pulumi/pulumi-google-native/sdk/go/google/iam/v1"
+	storage "github.com/pulumi/pulumi-google-native/sdk/go/google/storage/v1"
 	tpuv2 "github.com/pulumi/pulumi-google-native/sdk/go/google/tpu/v2"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
@@ -10,20 +12,61 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		conf := config.New(ctx, "")
 
-		node, err := tpuv2.NewNode(ctx, "examplenodeResourceResourceFromTpuv2", &tpuv2.NodeArgs{
-			NodeId:          pulumi.String("graphcast-tpu-esta"),
-			Location:        pulumi.String(conf.Require("location")),
-			AcceleratorType: pulumi.Sprintf("v5litepod-%d", conf.RequireInt("tpu-chip-core-number")),
-			RuntimeVersion:  pulumi.String("v2-tpuv5-litepod"),
-			NetworkConfig: tpuv2.NetworkConfigArgs{
-				EnableExternalIps: pulumi.Bool(true)},
-		})
-		if err != nil {
-			return err
+		if conf.GetBool("") {
+
+			// Create a TPU node
+			node, err := tpuv2.NewNode(ctx, "tpu", &tpuv2.NodeArgs{
+				NodeId:          pulumi.String("graphcast-tpu-esta"),
+				Location:        pulumi.String(conf.Require("location")),
+				AcceleratorType: pulumi.Sprintf("v5litepod-%d", conf.RequireInt("tpu-chip-core-number")),
+				RuntimeVersion:  pulumi.String("v2-tpuv5-litepod"),
+				NetworkConfig: tpuv2.NetworkConfigArgs{
+					EnableExternalIps: pulumi.Bool(true),
+				},
+			})
+			if err != nil {
+				return err
+			}
+
+			ctx.Export("nodeId", node.NodeId)
+			ctx.Export("nodeName", node.Name)
 		}
 
-		ctx.Export("nodeId", node.NodeId)
-		ctx.Export("name", node.Name)
+		if conf.GetBool("enableBucket") {
+
+			// Create a GCS bucket
+			bucket, err := storage.NewBucket(ctx, "bucket", &storage.BucketArgs{
+				Name:     pulumi.String("graphcast-tpu-blob-storage"),
+				Location: pulumi.String(conf.Require("location")),
+			})
+			if err != nil {
+				return err
+			}
+
+			// Create a Cloud TPU service account
+			serviceAccount, err := iam.NewServiceAccount(ctx, "tpuServiceAccount", &iam.ServiceAccountArgs{
+				AccountId:   pulumi.String("tpu-service-account"),
+				DisplayName: pulumi.String("TPU Service Account"),
+			})
+			if err != nil {
+				return err
+			}
+
+			// Grant the service account write permissions on the bucket
+			_, err = storage.NewBucketIamMember(ctx, "BucketIamBinding", &storage.BucketIamMemberArgs{
+				Name:   bucket.Name,
+				Role:   pulumi.String("roles/storage.objectCreator"),
+				Member: pulumi.Sprintf("serviceAccount:%s", serviceAccount.Email),
+			})
+			if err != nil {
+				return err
+			}
+
+			ctx.Export("bucketName", bucket.Name)
+			ctx.Export("serviceAccountEmail", serviceAccount.Email)
+
+			// mount the bucket
+		}
 
 		return nil
 	})
